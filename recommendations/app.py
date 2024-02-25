e028ac2b4319cf91d92db483dbdcee081848e9e9
mattreu <46345374+mattreu@users.noreply.github.com>
2024-02-25 11:51:30 +0100

    move recommendations to separate location

--- recommendations/app.py
+++ recommendations/app.py
@@ -1,14 +1,22 @@
 from flask import Flask, request, jsonify
 from alt_rbm import RBM
+from pathlib import Path
+import sys
 
 app = Flask(__name__)
 
-model = RBM()
+rmb_model = RBM()
+filename = "rbm_app.json"
+rbm_file_path = Path("rbm_models/"+filename)
+if not rbm_file_path.is_file():
+    sys.exit("No rbm model exists")
+else:
+    rmb_model.load_from_file(filename)
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()  # get data from POST request
-    recommendations = model.get_recommendations(data)  # make recommendations
+    recommendations = rmb_model.get_recommendations(data)  # make recommendations
     return jsonify({'recommendations': recommendations})  # return recommendations
 
 if __name__ == '__main__':
