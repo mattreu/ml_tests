5f7454ba5439c900cf5bb2fa0f6544cf07647329
mattreu <46345374+mattreu@users.noreply.github.com>
2024-02-29 12:43:09 +0100

    homepage display

--- recommendations/app.py
+++ recommendations/app.py
@@ -1,17 +1,28 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from alt_rbm import RBM
 from pathlib import Path
 import sys
+import os
 
 app = Flask(__name__)
 
+directory = os.path.dirname(os.path.abspath(__file__))
 rmb_model = RBM()
 filename = "rbm_app.json"
-rbm_file_path = Path("rbm_models/"+filename)
+rbm_file_path = Path(os.path.join(directory, "rbm_models", filename))
 if not rbm_file_path.is_file():
-    sys.exit("No rbm model exists")
+    sys.exit("No rbm model exists in " + str(rbm_file_path.absolute()))
 else:
     rmb_model.load_from_file(filename)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/initial', methods=['POST'])
+def initial():
+    recommendations = rmb_model.prepare_initial_recommendactions()
+    return recommendations
 
 @app.route('/predict', methods=['POST'])
 def predict():
