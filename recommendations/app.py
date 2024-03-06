09228fe1c23afcb1853e93872b60d8cdfd711670
mattreu <46345374+mattreu@users.noreply.github.com>
2024-03-06 15:17:33 +0100

    loading rbm models

--- recommendations/app.py
+++ recommendations/app.py
@@ -3,32 +3,58 @@ from alt_rbm import RBM
 from pathlib import Path
 import sys
 import os
+import glob
 
 app = Flask(__name__)
 
-directory = os.path.dirname(os.path.abspath(__file__))
 rmb_model = RBM()
-filename = "rbm_app.json"
-rbm_file_path = Path(os.path.join(directory, "rbm_models", filename))
-if not rbm_file_path.is_file():
-    sys.exit("No rbm model exists in " + str(rbm_file_path.absolute()))
-else:
-    rmb_model.load_from_file(filename)
+
+def get_models():
+    directory = os.path.dirname(os.path.abspath(__file__))
+    rbm_models_directory = os.path.join(directory, "rbm_models")
+    return [os.path.splitext(os.path.basename(filepath))[0] for filepath in glob.glob(rbm_models_directory + "/*")]
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    models = get_models()
+    context = {'models': models}
+    return render_template('home.html', context=context)
 
+@app.route('/load_rbm_model', methods=['POST'])
+def load_rbm_model():
+    data = request.get_json()
+    directory = os.path.dirname(os.path.abspath(__file__))
+    rmb_model = RBM()
+    filename = data['model'] + '.json'
+    rbm_file_path = Path(os.path.join(directory, "rbm_models", filename))
+    if not rbm_file_path.is_file():
+        return jsonify({'success': False, 'message': "Brak modelu w: " + str(rbm_file_path.absolute())})
+    else:
+        rmb_model.load_from_file(filename)
+    return jsonify({'success': True, 'message': "Model poprawnie wczytany"})
+
+# actions for chosen model(s)
+@app.route('/actions')
+def actions():
+    return render_template('actions.html')
+
+# initial data gathering to recommend to new user
 @app.route('/initial', methods=['POST'])
 def initial():
     recommendations = rmb_model.prepare_initial_recommendactions()
     return recommendations
 
+# test users to choose from
+@app.route('/prepared', methods=['POST'])
+def prepared():
+    return render_template('prepared.html')
+
+# get recommendations
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json()  # get data from POST request
-    recommendations = rmb_model.get_recommendations(data)  # make recommendations
-    return jsonify({'recommendations': recommendations})  # return recommendations
+    data = request.get_json()
+    recommendations = rmb_model.get_recommendations(data)
+    return jsonify({'recommendations': recommendations})
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
--- recommendations/static/script.js
+++ recommendations/static/script.js
@@ -1,1 +1,53 @@
-console.log("Script loaded");
+document.addEventListener("DOMContentLoaded", () => {
+    // load models and actions
+    prepareModelsChoice()
+})
+
+const prepareModelsChoice = () => {
+    const modelSelection = document.querySelector("#model_selection")
+    modelSelection.addEventListener('change', event => {
+        const selectedOption = event.target.value
+        const data = {
+            model: selectedOption
+        }
+        fetch('./load_rbm_model',{
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        }).then(response => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                throw new Error('Error: ' + response.statusText)
+            }
+        }).then(data => {
+            if(data.success==true){
+                // wczytany model, wyświetlenie akcji
+                fetch('./actions')
+                .then(response => {
+                    if (response.ok) {
+                        return response.text()
+                    } else {
+                        throw new Error('Error: ' + response.statusText)
+                    }
+                }).then(data => {
+                    document.getElementById('content').innerHTML = data
+                })
+            }
+            else{
+                throw new Error('Error: ' + data.message)
+            }
+        })
+        
+    })
+}
+
+const newUser = () => {
+
+}
+
+const existingUser = () => {
+
+}
