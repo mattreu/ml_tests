4569c495d3a99c21cdc211db998b89b2327f74c7
mattreu <46345374+mattreu@users.noreply.github.com>
2024-05-16 14:01:46 +0200

    run multiple mf models

--- recommendations/run_multiple_models.py
+++ recommendations/run_multiple_models.py
@@ -1,5 +1,7 @@
 from multiprocessing import Pool
+from functools import partial
 from alt_rbm import RBM
+from alt_matrix_factor import MatrixFactorization
 
 def run_rbm(rbm: RBM):
     error = rbm.train(return_error=True)
@@ -8,4 +10,13 @@ def run_rbm(rbm: RBM):
 def run_rbms(rbms: list):
     with Pool() as p:
         results = p.map(run_rbm, rbms)
+    return results
+
+def run_mf(mf: MatrixFactorization):
+    error = mf.train()
+    return mf, error
+
+def run_mfs(mfs: list):
+    with Pool() as p:
+        results = p.map(run_mf, mfs)
     return results
